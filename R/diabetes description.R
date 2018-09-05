8cba6778d9311c2da5e2ac22e5850c16de1d436f
Shakeel Gavioli-Akilagun <sgavioli-akilagun@nas156-183.york.ac.uk>
2018-09-05 13:46:21 +0100

    first vignette commit

---
+++ R/diabetes description.R
@@ -0,0 +1,11 @@
+#' Virginia School of Medicine diabetes dataset 
+#' 
+#' The data consist of 17 variables on 373 subjects from 1046 subjects who were interviewed in a study to understand the prevalence of obesity, diabetes, and other cardiovascular risk factors in central Virginia for African Americans. According to Dr John Hong, Diabetes Mellitus Type II (adult onset diabetes) is associated most strongly with obesity. The waist/hip ratio may be a predictor in diabetes and heart disease. DM II is also agssociated with hypertension - they may both be part of "Syndrome X". The 373 subjects were the ones who were actually screened for diabetes, minus subjects with missing values in the original dataset. Glycosolated hemoglobin > 7.0 is usually taken as a positive diagnosis of diabetes. 
+#' 
+#' @docType data
+#' @usage data(Diabetes)
+#' @references Willems JP, Saunders JT, DE Hunt, JB Schorling: Prevalence of coronary heart disease risk factors among rural blacks: A community-based study. Southern Medical Journal 90:814-820; 1997 
+#' @references Schorling JB, Roach J, Siegel M, Baturka N, Hunt DE, Guterbock TM, Stewart HL: A trial of church-based smoking cessation interventions for rural African Americans. Preventive Medicine 26:92-101; 1997.
+#' @source \href{http://biostat.mc.vanderbilt.edu/wiki/Main/DataSets}{Vanderbilt department of Biostatistics free to use datasets}
+#' @export
+"Diabetes"
